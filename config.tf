742787b3ac064e25eb11137df7b59c7e196eae7c
agorbanev <gorbanev.am@ya.ru>
2023-03-09 16:04:12 +0300

    0.0.2

--- config.tf
+++ config.tf
@@ -43,6 +43,56 @@ resource "yandex_compute_instance" "vm-test1" {
 
 }
 
+resource "yandex_compute_instance" "vm-test1" {
+  name = "test2"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_image.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet_terraform.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yml")}"
+  }
+
+}
+
+resource "yandex_compute_instance" "vm-test2" {
+  name = "test3"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_image.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet_terraform.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yml")}"
+  }
+
+}
+
 resource "yandex_vpc_network" "network_terraform" {
   name = "net_terraform"
 }
@@ -53,79 +103,3 @@ resource "yandex_vpc_subnet" "subnet_terraform" {
   network_id     = yandex_vpc_network.network_terraform.id
   v4_cidr_blocks = ["192.168.15.0/24"]
 }
-
-# resource "yandex_compute_instance" "vm-1" {
-#   name = "terraform1"
-
-#   resources {
-#     cores  = 2
-#     memory = 2
-#   }
-
-#   boot_disk {
-#     initialize_params {
-#       image_id = "fd87va5cc00gaq2f5qfb"
-#     }
-#   }
-
-#   network_interface {
-#     subnet_id = yandex_vpc_subnet.subnet-1.id
-#     nat       = true
-#   }
-
-#   metadata = {
-#     ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
-#   }
-# }
-
-# resource "yandex_compute_instance" "vm-2" {
-#   name = "terraform2"
-
-#   resources {
-#     cores  = 4
-#     memory = 4
-#   }
-
-#   boot_disk {
-#     initialize_params {
-#       image_id = "fd87va5cc00gaq2f5qfb"
-#     }
-#   }
-
-#   network_interface {
-#     subnet_id = yandex_vpc_subnet.subnet-1.id
-#     nat       = true
-#   }
-
-#   metadata = {
-#     ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
-#   }
-# }
-
-# resource "yandex_vpc_network" "network-1" {
-#   name = "network1"
-# }
-
-# resource "yandex_vpc_subnet" "subnet-1" {
-#   name           = "subnet1"
-#   zone           = "ru-central1-a"
-#   network_id     = yandex_vpc_network.network-1.id
-#   v4_cidr_blocks = ["192.168.10.0/24"]
-# }
-
-# output "internal_ip_address_vm_1" {
-#   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-# }
-
-# output "internal_ip_address_vm_2" {
-#   value = yandex_compute_instance.vm-2.network_interface.0.ip_address
-# }
-
-
-# output "external_ip_address_vm_1" {
-#   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-# }
-
-# output "external_ip_address_vm_2" {
-#   value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
-# }
