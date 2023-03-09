ff76be42d841a4da7b8acf7f1cf5bc5a6cf0f8e0
agorbanev <gorbanev.am@ya.ru>
2023-03-09 15:46:05 +0300

    0.0.1

--- config.tf
+++ config.tf
@@ -50,7 +50,7 @@ resource "yandex_vpc_network" "network_terraform" {
 resource "yandex_vpc_subnet" "subnet_terraform" {
   name           = "sub_terraform"
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.default.id
+  network_id     = yandex_vpc_network.network_terraform.id
   v4_cidr_blocks = ["192.168.15.0/24"]
 }
 
